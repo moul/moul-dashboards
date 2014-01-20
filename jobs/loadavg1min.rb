41b692f41a51dd0158dd325ec4cdb8ab59afe79a
Manfred Touron <m@42.am>
2014-01-20 10:05:35 +0100

    fixed loadavg for osx

--- jobs/loadavg1min.rb
+++ jobs/loadavg1min.rb
@@ -5,11 +5,12 @@ points = []
 end
 last_x = points.last[:x]
 
-SCHEDULER.every '1m' do
+SCHEDULER.every '1m', :first_in => 0 do
   points.shift
   last_x += 1
   uptime = %x('uptime')
-  points << { x: last_x, y: uptime[-17..-14].to_f }
+  loadavg1min = uptime.split(":")[-1].strip().split(" ")[0]
+  points << { x: last_x, y: loadavg1min.to_f }
 
   send_event('loadavg1min', points: points)
 end
